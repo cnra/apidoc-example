368951daa76d7dfd98b3f7b48c5fa7cc1599c3f7
rrr <roser137@hotmail.com>
2020-08-23 17:38:03 +0300

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+/**
+ * @api {get} /user/:id Kullanici bilgilerini getirir
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id getirilecek kullanici id si.
+ *
+ * @apiSuccess {String} firstname Kullanici adi
+ * @apiSuccess {String} lastname  Kullanici soyadi.
+ */
+
+
+function GetUser() {
+   return 0;
+}
